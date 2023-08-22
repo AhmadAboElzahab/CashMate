b853a7e0b0c5499a31c8d2b3c7181707e1419bfe
BabaGanoush <server.vbs@gmail.com>
2023-08-22 14:11:40 +0300

    Setting up server and connection to database

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,19 @@
-const express = require("express")
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const app = express();
+require('dotenv').config();
+app.use(express.json());
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Coneected on http://localhost:${process.env.PORT}/`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
