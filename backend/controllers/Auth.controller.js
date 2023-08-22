7aabbd4dc3a3b8508309488c7869c4de74cf0f53
BabaGanoush <server.vbs@gmail.com>
2023-08-22 14:22:59 +0300

    adding auth controller

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -1,88 +1,57 @@
-const Promo = require('../models/Promo.model');
-const couponCode = require('coupon-code');
+const User = require('../models/user.model');
 
-async function getAllPromoCodes(req, res) {
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const login = async (req, res) => {
   try {
-    const promoCodes = await Promo.find();
-    res.status(200).json(promoCodes);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-}
+    const { email, password } = req.body;
 
-async function createPromoCode(req, res) {
-  try {
-    const { value } = req.body;
-    if (value === '') return res.status(400).json({ error: 'value Should not be empty' });
-
-    const promo = new Promo({
-      code: couponCode.generate({ parts: 1 }),
-      value,
-    });
-
-    await promo.save();
-
-    res.status(200).json({ data: promo });
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-}
-
-async function deletePromoCode(req, res) {
-  try {
-    const promoId = req.params.id;
-    const deletedPromo = await Promo.findByIdAndDelete(promoId);
-
-    if (!deletedPromo) {
-      return res.status(404).json({ error: 'Promo not found' });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json({ message: 'Promo Code Deleted successfully' });
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-}
-
-async function updatePromoCode(req, res) {
-  try {
-    const promoId = req.params.id;
-    const { value } = req.body;
-
-    if (value === '') return res.status(400).json({ error: 'value Should not be empty' });
-
-    const updatedPromo = await Promo.findByIdAndUpdate(promoId, { value }, { new: true });
-
-    if (!updatedPromo) {
-      return res.status(404).json({ error: 'Promo not found' });
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
 
-    res.status(200).json({ data: updatedPromo });
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-}
-
-async function checkPromoCode(req, res) {
-  try {
-    const { amount, code } = req.body;
-    const findCode = await Promo.findOne({ code });
-
-    if (findCode) {
-      const newAmount = amount - (findCode.value * amount) / 100;
-      res.json({ price: newAmount });
-    } else {
-      res.status(404).json({ error: 'Promo code not found' });
-    }
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.SECRET_KEY);
+    res
+      .cookie('token', token, { httpOnly: true, secure: true })
+      .json({ name: user.name, role: user.role, UserId: user._id })
+      .status(200);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ message: 'Internal server error' });
   }
-}
-
-module.exports = {
-  getAllPromoCodes,
-  createPromoCode,
-  deletePromoCode,
-  updatePromoCode,
-  checkPromoCode,
 };
+
+const register = async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: 'email already in use' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const logout = async (req, res) => {
+  res.cookie('token', '', { expires: new Date(0), httpOnly: true });
+  res.json({ message: 'Logout successful' });
+};
+
+module.exports = { login, register, logout };
