4a9dfce0e5fcb63f4eae25237a740b6657738aa0
BabaGanoush <server.vbs@gmail.com>
2023-08-22 14:38:11 +0300

    add auth middleware

---
+++ backend/middlewares/Auth.middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const Authorization = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization token missing' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    req.userId = req.user.id;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = adminAuthorization;
